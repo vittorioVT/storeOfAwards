d87742d90caeab26e53c84f143ffa9aec8533811
Viktor Tiutiun <vvtiutiun@googlemail.com>
2021-01-26 11:17:39 +0200

    Conected material-angular and created materialAngular.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from "@angular/core";
 import { BootstrapModule } from "./bootstrap.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { MaterialAngularModule } from "./materialAngular.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BootstrapModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BootstrapModule,
+    MaterialAngularModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
