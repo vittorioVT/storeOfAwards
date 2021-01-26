30eb37ca224edde980be738efffd2cff45e0ef27
Viktor Tiutiun <vvtiutiun@googlemail.com>
2021-01-26 10:43:52 +0200

    Connected bootstrap and created bootstrap.module.ts

---
+++ src/app/bootstrap.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { ButtonsModule } from "ngx-bootstrap/buttons";
+
+@NgModule({
+  declarations: [],
+  imports: [ButtonsModule.forRoot()],
+  exports: [],
+  providers: []
+})
+export class BootstrapModule {}
